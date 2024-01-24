84b300b85db991f85bea0fa901ca123101e5b298
Rafael Vesoloski <rafaeljosevesoloski206@gmail.com>
2024-01-24 16:54:15 -0300

    Primeiro commit

---
+++ 02-eventos/index.js
@@ -0,0 +1,10 @@
+function onMouseEnter() {
+    console.log('mouse enter')
+}
+
+function onClick() {
+    console.log('Clique do elemento')
+}
+
+
+//Faz aqui as funções e chama no html.
---
+++ 04-obtendo-elementos/index.js
@@ -0,0 +1,25 @@
+
+function show() {
+    // Obter um elemento único a partir do seu id
+    const contactList = document.getElementById('contact-list')
+    console.log(contactList)
+
+    // Celeciona pega tag
+    const listElements = document.getElementsByTagName('li')
+    console.log(listElements)
+
+    // celeciona pelo nome da classe
+    const contactInputs  = document.getElementsByClassName('contact-input')
+    console.log(contactInputs)
+    
+    const contact = document.querySelectorAll('#contact-list > li > label')
+    console.log(contact)
+
+    //pelo nome
+    const contact1 = document.getElementsByName("contact1")
+    console.log(contact1)
+
+    const firsContact = document.querySelector('#contact-list > li > label')
+    console.log(firsContact)
+
+  }
